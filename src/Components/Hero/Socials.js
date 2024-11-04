50d31efab3c3bc786a4f6cb5ef05878ec7352a92
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-04 18:12:25 +0200

    Updated socials.js
    changed link to  an icon

--- src/Components/Hero/Socials.js
+++ src/Components/Hero/Socials.js
@@ -4,7 +4,7 @@ import "./Socials.css"
 function socials() {
   return (
     <div className="home_social">
-        <a href="#" className='home_social-icon' target='_blank'><i class="uil uil-github"></i></a>
+        <a href="example.com" className='home_social-icon' target='_blank'><i class="uil uil-github"></i></a>
 
         <a href="https://www.linkedin.com/feed/" className='home_social-icon' target='_blank'><i class="uil uil-linkedin"></i></a>
 
