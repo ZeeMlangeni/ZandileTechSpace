414b7991072412fc257e264bcdbd8008170e7fc1
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-04 18:14:46 +0200

    Update social.js
    updating the social js link to noreferrer

--- src/Components/Hero/Socials.js
+++ src/Components/Hero/Socials.js
@@ -6,9 +6,12 @@ function socials() {
     <div className="home_social">
         <a href="example.com" className='home_social-icon' target='_blank'><i class="uil uil-github"></i></a>
 
-        <a href="https://www.linkedin.com/feed/" className='home_social-icon' target='_blank'><i class="uil uil-linkedin"></i></a>
+        <a href="https://www.linkedin.com/feed/" className='home_social-icon' target='_blank' rel='noreferrer'>
+  <i className="uil uil-linkedin"></i>
+</a>
 
-        <a href="https://www.instagram.com/zeeicygirl/" className='home_social-icon' target='_blank'><i class="uil uil-instagram-alt"></i></a>
+
+        <a href="https://www.instagram.com/zeeicygirl/" className='home_social-icon' target='_blank' rel='noreferrer'><i class="uil uil-instagram-alt"></i></a>
     </div>
   )
 }
