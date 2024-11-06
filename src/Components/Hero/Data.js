9b8a4915af847d52e0e5c35ccd12b05ae4431228
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-06 21:42:29 +0200

    Update data js
    changed stuff

--- src/Components/Hero/Data.js
+++ src/Components/Hero/Data.js
@@ -1,25 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 function Data() {
   return (
-    <div className='home__data'>
-        <h1 className='home__title'>Hi I'm <span className='letter'>Zandile</span><br/>  <span className='letter'>Front-end</span> Developer</h1>
-<p className='home__description'>I am front-end developer in training with a love for building engaging websites. I'm actively working on projects that showcase my skills in HTML, CSS, JavaScript, and React and much more.</p>
-
-<div className='reach-me'>
-<button className='Touch-btn'>Get in Touch
-<svg className="send-svg"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"   id="message"><path fill="#ffff" d="M20.34,9.32l-14-7a3,3,0,0,0-4.08,3.9l2.4,5.37h0a1.06,1.06,0,0,1,0,.82l-2.4,5.37A3,3,0,0,0,5,22a3.14,3.14,0,0,0,1.35-.32l14-7a3,3,0,0,0,0-5.36Zm-.89,3.57-14,7a1,1,0,0,1-1.35-1.3l2.39-5.37A2,2,0,0,0,6.57,13h6.89a1,1,0,0,0,0-2H6.57a2,2,0,0,0-.08-.22L4.1,5.41a1,1,0,0,1,1.35-1.3l14,7a1,1,0,0,1,0,1.78Z"></path></svg>
-</button>
-<span className="avail-dot">
-    <span className="dot"></span>
-    <p>Available now</p>
-</span>
-</div>
-
-
-
-    </div>
-  )
+    <section className='hero'>
+      <h1 className='hero__title'>
+        Driven by Purpose, Empowered by Tech
+      </h1>
+      <p className='hero__description'>
+        I'm Zandile, a front-end developer in training who believes technology can be a force for good. Through every line of code, I’m learning how to create impactful, engaging digital experiences—and I’m just getting started.
+      </p>
+      <p className='hero__description'>
+        From HTML to React, my journey is all about growth, curiosity, and using tech to uplift lives. Grateful for every opportunity to learn, build, and make a difference. Let's bring the future to life, one project at a time.
+      </p>
+      
+      <div className='reach-me'>
+        <button className='hero-button'>
+          Explore My Work
+        </button>
+        <span className="avail-dot">
+          <span className="dot"></span>
+          <p>Available now</p>
+        </span>
+      </div>
+    </section>
+  );
 }
 
-export default Data
+export default Data;
