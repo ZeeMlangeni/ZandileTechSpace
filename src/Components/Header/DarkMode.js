c94e3dc83f5c39569de6340024c3e8e8e6989326
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-06 16:38:06 +0200

    Update darkmode
    made changes

--- src/Components/Header/DarkMode.js
+++ src/Components/Header/DarkMode.js
@@ -1,36 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ReactComponent as Sun } from '../../Assets/Sun.svg';
 import { ReactComponent as Moon } from '../../Assets/Moon.svg';
-
-
-
 import "./DarkMode.css";
 
 const DarkMode = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
-    const setDarkMode=() =>{
-        document.querySelector("body").setAttribute('data-theme', 'dark')
-    }
+    useEffect(() => {
+        // Check if dark mode is active on page load
+        const currentTheme = document.body.getAttribute('data-theme');
+        setIsDarkMode(currentTheme === 'dark');
+    }, []);
 
-    const setLightMode=() =>{
-        document.querySelector("body").setAttribute('data-theme', 'light')
-    }
+    const setDarkMode = () => {
+        document.querySelector("body").setAttribute('data-theme', 'dark');
+        setIsDarkMode(true); // Update state when dark mode is set
+    };
 
-    const toggleTheme =(e) =>{
+    const setLightMode = () => {
+        document.querySelector("body").setAttribute('data-theme', 'light');
+        setIsDarkMode(false); // Update state when light mode is set
+    };
+
+    const toggleTheme = (e) => {
         if (e.target.checked) setDarkMode();
         else setLightMode();
-    }
-  
+    };
+
     return (
         <div className='dark_mode'>
             <input
                 className='dark_mode_input'
                 type='checkbox'
                 id='darkmode-toggle'
-             
+                checked={isDarkMode}
                 onChange={toggleTheme}
             />
-            <label className='dark_mode_label' for='darkmode-toggle'>
+            <label className='dark_mode_label' htmlFor='darkmode-toggle'>
                 <Sun />
                 <Moon />
             </label>
