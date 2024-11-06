d25ce93c52aabc1f51b49ebaa40c682aab061f6d
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-06 22:09:14 +0200

    Updated class to classname
    updated icons

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,11 +8,11 @@ function Footer() {
 <div className="home-social ">
       <ul className='link-list'>
 
-      <a href="example.com" className='home-social-icon' target='_blank' rel='noreferrer'><i class="uil uil-github"></i></a>
+      <a href="example.com" className='home-social-icon' target='_blank' rel='noreferrer'><i className="uil uil-github"></i></a>
 
-<a href="https://www.linkedin.com/feed/" className='home-social-icon' target='_blank' rel='noreferrer'><i class="uil uil-linkedin"></i></a>
+<a href="https://www.linkedin.com/feed/" className='home-social-icon' target='_blank' rel='noreferrer'><i className="uil uil-linkedin"></i></a>
 
-<a href="https://www.instagram.com/zeeicygirl/" className='home-social-icon' target='_blank' rel='noreferrer'><i class="uil uil-instagram-alt"></i></a>
+<a href="https://www.instagram.com/zeeicygirl/" className='home-social-icon' target='_blank' rel='noreferrer'><i className="uil uil-instagram-alt"></i></a>
 
         <li className='links'><a href="example.com">Home</a></li>
         <li className='links'><a href="example.com">About</a></li>
--- src/Components/Hero/Socials.js
+++ src/Components/Hero/Socials.js
@@ -4,14 +4,14 @@ import "./Socials.css"
 function socials() {
   return (
     <div className="home_social">
-        <a href="example.com" className='home_social-icon' target='_blank' rel='noreferrer'><i class="uil uil-github"></i></a>
+        <a href="example.com" className='home_social-icon' target='_blank' rel='noreferrer'><i className="uil uil-github"></i></a>
 
         <a href="https://www.linkedin.com/feed/" className='home_social-icon' target='_blank' rel='noreferrer'>
   <i className="uil uil-linkedin"></i>
 </a>
 
 
-        <a href="https://www.instagram.com/zeeicygirl/" className='home_social-icon' target='_blank' rel='noreferrer'><i class="uil uil-instagram-alt"></i></a>
+        <a href="https://www.instagram.com/zeeicygirl/" className='home_social-icon' target='_blank' rel='noreferrer'><i className="uil uil-instagram-alt"></i></a>
     </div>
   )
 }
