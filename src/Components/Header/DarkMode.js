a13acb9530add811ca98970631e7f990235ef678
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-06 16:02:15 +0200

    update dark mode
    added state

--- src/Components/Header/DarkMode.js
+++ src/Components/Header/DarkMode.js
@@ -1,38 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ReactComponent as Sun } from '../../Assets/Sun.svg';
 import { ReactComponent as Moon } from '../../Assets/Moon.svg';
-
-
 
 import "./DarkMode.css";
 
 const DarkMode = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
-    const setDarkMode=() =>{
-        document.querySelector("body").setAttribute('data-theme', 'dark')
+    useEffect(() => {
+        // Check the current theme on load and set state accordingly
+        const currentTheme = document.body.getAttribute('data-theme');
+        setIsDarkMode(currentTheme === 'dark');
+    }, []);
+
+    const toggleTheme = () => {
+        const newTheme = isDarkMode ? 'light' : 'dark';
+        document.querySelector("body").setAttribute('data-theme', newTheme);
+        setIsDarkMode(!isDarkMode);
     }
 
-    const setLightMode=() =>{
-        document.querySelector("body").setAttribute('data-theme', 'light')
-    }
-
-    const toggleTheme =(e) =>{
-        if (e.target.checked) setDarkMode();
-        else setLightMode();
-    }
-  
     return (
         <div className='dark_mode'>
             <input
                 className='dark_mode_input'
                 type='checkbox'
                 id='darkmode-toggle'
-             
+                checked={isDarkMode}
                 onChange={toggleTheme}
             />
-            <label className='dark_mode_label' for='darkmode-toggle'>
-                <Sun />
-                <Moon />
+            <label className='dark_mode_label' htmlFor='darkmode-toggle'>
+                <Sun className={`sun_icon ${isDarkMode ? 'hidden' : ''}`} />
+                <Moon className={`moon_icon ${!isDarkMode ? 'hidden' : ''}`} />
             </label>
         </div>
     );
