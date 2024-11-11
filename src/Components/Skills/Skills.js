97891bfb4bbf5b4095d4ff29d1da40d23e05cd35
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-11 22:21:50 +0200

    Updated skills

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,30 +1,37 @@
-import React from 'react'
-import "./Skills.css"
-import Languages from './Icons/Languages'
-import Framework from './Icons/Framework'
-import Tools from './Icons/Tools'
-import Soft from './Icons/Soft'
+import React from 'react';
+import './Skills.css';
+import Languages from './Icons/Languages';
+import Framework from './Icons/Framework';
+import Tools from './Icons/Tools';
+import Soft from './Icons/Soft';
 
 function Skills() {
   return (
-    <div className='skills section container ' id="skills">
-
-<h1 className='skills__title'>My Skills</h1>
-
-<div className='row'>
-  
-<Languages/>
-<Framework/>
-<Tools/>
-<Soft/>
-</div>
-
-
-
-</div>
-
-   
-  )
+    <div className="skills section container" id="skills">
+      <h1 className="skills__title">My Skills</h1>
+      <p className="skills__description">
+        Here's a collection of my technical expertise and the tools I use to build amazing things.
+      </p>
+      <div className="skills__icons">
+        <div className="skills__icon">
+          <Languages />
+          <h3>Languages</h3>
+        </div>
+        <div className="skills__icon">
+          <Framework />
+          <h3>Frameworks</h3>
+        </div>
+        <div className="skills__icon">
+          <Tools />
+          <h3>Tools</h3>
+        </div>
+        <div className="skills__icon">
+          <Soft />
+          <h3>Soft Skills</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Skills
+export default Skills;
