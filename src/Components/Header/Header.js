42f4fefa3c0031fb98300920e8746730bf7eb18e
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-11 17:53:30 +0200

    changed rems

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -16,7 +16,7 @@ function Header() {
   };
 
   return (
-    <header className="header" >
+    <header className="header container grid " >
       <nav className="nav container">
         <a href="index.html" className="nav logo">
           <h2>
