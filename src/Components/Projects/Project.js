285e936985f8331ae614691b559461d3c890dd02
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-12 00:08:25 +0200

    updated

--- src/Components/Projects/Project.js
+++ src/Components/Projects/Project.js
@@ -1,26 +1,25 @@
-import React from 'react'
-import "./Project.css"
+import React from 'react';
+import './Project.css';
 
 function Project(props) {
   return (
-    <div className='project section container'>
-
-
-
-<div className='project__card gird'>
-  
-<img className="projectImg"src={props.image} alt='image-of-project'></img>
-      <h2 className='project__title'>{props.name}</h2>
-      <p className='project__desc'>{props.description}</p>
-      <p className='techs'>Technologies Used<br/>{props.technologies}</p>
-      <button className='git-btn'>Git repository</button>
-      <button className='visit-btn'>Visit Site</button>
-   
-</div>
-
+    <div className="project section container">
+      <div className="project__card">
+        <img className="projectImg" src={props.image} alt="image-of-project" />
+        <h2 className="project__title">{props.name}</h2>
+        <p className="project__desc">{props.description}</p>
+        <p className="techs">
+          Technologies Used
+          <br />
+          {props.technologies}
+        </p>
+        <div className="buttons">
+          <button className="git-btn">Git repository</button>
+          <button className="visit-btn">Visit Site</button>
+        </div>
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default Project
+export default Project;
