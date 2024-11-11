e875df4637abf262746e7d2776a2a4e13030ae03
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-11 23:12:44 +0200

    updated skills

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -8,11 +8,12 @@ import Soft from './Icons/Soft';
 function Skills() {
   return (
     <div className="skills section container" id="skills">
-      <h1 className="skills__title">My Skills</h1>
-      <p className="skills__description">
-        Here's a collection of my technical expertise and the tools I use to build amazing things.
-      </p>
-      <div className="skills__icons">
+    <h1 className="skills__title">My Skills</h1>
+    <p className="skills__description">
+      Here's a collection of my technical expertise and the tools I use to build amazing things.
+    </p>
+    <div className="skills__icons">
+      <div className="skills__block">
         <div className="skills__category">
           <h3 className="skills__category-title">Languages</h3>
           <Languages />
@@ -21,6 +22,8 @@ function Skills() {
           <h3 className="skills__category-title">Frameworks</h3>
           <Framework />
         </div>
+      </div>
+      <div className="skills__block">
         <div className="skills__category">
           <h3 className="skills__category-title">Tools</h3>
           <Tools />
@@ -31,6 +34,8 @@ function Skills() {
         </div>
       </div>
     </div>
+  </div>
+  
   );
 }
 
