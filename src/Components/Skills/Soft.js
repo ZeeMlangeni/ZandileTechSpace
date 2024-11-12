759a0dcbfd3f43c4c83e2042d8b002ba8e3b4973
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-12 12:13:16 +0200

    updated soft

---
+++ src/Components/Skills/Soft.js
@@ -0,0 +1,91 @@
+import React from 'react'
+
+function Soft() {
+  return (
+    <div className="skills__content">
+    <h3 className='skills__title'>nn</h3>
+    <div className='skills__box'>
+<div className="skills__group">
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 ClassName= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
+
+
+
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 ClassName= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
+
+
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 ClassName= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
+
+
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 ClassName= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
+
+    </div>
+
+    <div className="skills__group">
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 ClassName= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
+
+
+
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 ClassName= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
+
+
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 ClassName= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
+
+
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 ClassName= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
+
+    </div>
+
+</div>
+</div>
+  )
+}
+
+export default Soft
