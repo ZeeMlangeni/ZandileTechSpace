67b46ebd4fe112274df8c427b96fa1c93955fccd
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-12 14:18:27 +0200

    updated info

---
+++ src/Components/About-section/info.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function info() {
+  return (
+   
+<div className='about__info grid'>
+    <div className='about__box'>
+        <h3 className='about__subtitle'></h3>
+        <span className ='about__subtitle'></span>
+    </div>
+
+    <div className='about__box'>
+        <h3 className='about__subtitle'></h3>
+        <span className ='about__subtitle'></span>
+    </div>
+
+    <div className='about__box'>
+        <h3 className='about__subtitle'></h3>
+        <span className ='about__subtitle'></span>
+    </div>
+</div>
+
+   
+  )
+}
+
+export default info
