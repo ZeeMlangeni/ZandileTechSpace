54437708b97a5a54783123028c7c5c583203e86b
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-12 12:29:50 +0200

    Updated soft skills

--- src/Components/Skills/Soft.js
+++ src/Components/Skills/Soft.js
@@ -3,88 +3,78 @@ import React from 'react'
 function Soft() {
   return (
     <div className="skills__content">
-    <h3 className='skills__title'>nn</h3>
-    <div className='skills__box'>
-<div className="skills__group">
-<div className="skills__data">
-<i className='bx bxs-badge-check'></i>
-<div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
-</div>
-</div>
-
-
-
-<div className="skills__data">
-<i className='bx bxs-badge-check'></i>
-<div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
-</div>
-</div>
-
-
-<div className="skills__data">
-<i className='bx bxs-badge-check'></i>
-<div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
-</div>
-</div>
-
-
-<div className="skills__data">
-<i className='bx bxs-badge-check'></i>
-<div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
-</div>
-</div>
-
+    <h3 className="skills__title">Soft Skills</h3>
+    <div className="skills__box">
+      <div className="skills__group">
+        <div className="skills__data">
+          <i className='bx bxs-badge-check'></i>
+          <div>
+            <h3 className="skills__name">Communication</h3>
+            <span className="skills__level">Excellent</span>
+          </div>
+        </div>
+  
+        <div className="skills__data">
+          <i className='bx bxs-badge-check'></i>
+          <div>
+            <h3 className="skills__name">Problem-Solving</h3>
+            <span className="skills__level">Advanced</span>
+          </div>
+        </div>
+  
+        <div className="skills__data">
+          <i className='bx bxs-badge-check'></i>
+          <div>
+            <h3 className="skills__name">Teamwork</h3>
+            <span className="skills__level">Proficient</span>
+          </div>
+        </div>
+  
+        <div className="skills__data">
+          <i className='bx bxs-badge-check'></i>
+          <div>
+            <h3 className="skills__name">Adaptability</h3>
+            <span className="skills__level">High</span>
+          </div>
+        </div>
+      </div>
+  
+      <div className="skills__group">
+        <div className="skills__data">
+          <i className='bx bxs-badge-check'></i>
+          <div>
+            <h3 className="skills__name">Time Management</h3>
+            <span className="skills__level">Strong</span>
+          </div>
+        </div>
+  
+        <div className="skills__data">
+          <i className='bx bxs-badge-check'></i>
+          <div>
+            <h3 className="skills__name">Critical Thinking</h3>
+            <span className="skills__level">Advanced</span>
+          </div>
+        </div>
+  
+        <div className="skills__data">
+          <i className='bx bxs-badge-check'></i>
+          <div>
+            <h3 className="skills__name">Leadership</h3>
+            <span className="skills__level">Intermediate</span>
+          </div>
+        </div>
+  
+        <div className="skills__data">
+          <i className='bx bxs-badge-check'></i>
+          <div>
+            <h3 className="skills__name">Empathy</h3>
+            <span className="skills__level">High</span>
+          </div>
+        </div>
+      </div>
     </div>
-
-    <div className="skills__group">
-<div className="skills__data">
-<i className='bx bxs-badge-check'></i>
-<div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
-</div>
-</div>
-
-
-
-<div className="skills__data">
-<i className='bx bxs-badge-check'></i>
-<div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
-</div>
-</div>
-
-
-<div className="skills__data">
-<i className='bx bxs-badge-check'></i>
-<div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
-</div>
-</div>
-
-
-<div className="skills__data">
-<i className='bx bxs-badge-check'></i>
-<div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
-</div>
-</div>
-
-    </div>
-
-</div>
-</div>
+  </div>
+  
   )
 }
 
