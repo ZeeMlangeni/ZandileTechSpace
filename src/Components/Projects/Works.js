d5833ddfc075918c50fd8c59036d0c2817b08a65
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-12 19:33:30 +0200

    updated works useeffect

--- src/Components/Projects/Works.js
+++ src/Components/Projects/Works.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Works.css'
 
 import {projectData} from './Datas'
@@ -6,6 +6,21 @@ import { projectNav } from './Datas'
 import WorkItems from './WorkItems'
 
 const Works=()=> {
+    const [item ,setItem] =useState({name: 'all'})
+    const [projects, setProjects]=useState([])
+    const [active,setActive] =useState(0)
+
+    useEffect(() =>{
+        if (item.name === 'all'){
+            setProjects(projectData)
+        }
+
+        else {
+            const newProject =projectData.filter((project)=>{
+                return project.category === item.name;
+            })
+        }
+    })
 
   return (
 
