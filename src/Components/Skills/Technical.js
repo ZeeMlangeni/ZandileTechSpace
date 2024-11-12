6e64656d9f8e3742e5ebe8f714f9997f710a4123
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-12 11:52:37 +0200

    updated technical

---
+++ src/Components/Skills/Technical.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Technical() {
+  return (
+    <div>
+
+
+        <div className="skills__content">
+            <h3 className='skills__title'></h3>
+
+        </div>
+    </div>
+  )
+}
+
+export default Technical
