c6279f1a0becde5041386e866619b8735513480a
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-12 12:16:40 +0200

    update technical

--- src/Components/Skills/Technical.js
+++ src/Components/Skills/Technical.js
@@ -5,89 +5,89 @@ function Technical() {
    
 
 
-        <div className="skills__content">
-            <h3 className='skills__title'>nn</h3>
-            <div className='skills__box'>
+    <div className="skills__content">
+    <h3 className='skills__title'>nn</h3>
+    <div className='skills__box'>
 <div className="skills__group">
-    <div className="skills__data">
-        <i className='bx bxs-badge-check'></i>
-        <div>
-            <h3 ClassName= 'skills__name'>dd</h3>
-            <span className='skills__level'></span>
-        </div>
-        </div>
-   
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 className= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
 
 
-     <div className="skills__data">
-        <i className='bx bxs-badge-check'></i>
-        <div>
-            <h3 ClassName= 'skills__name'>dd</h3>
-            <span className='skills__level'></span>
-        </div>
-        </div>
-    
 
-     <div className="skills__data">
-        <i className='bx bxs-badge-check'></i>
-        <div>
-            <h3 ClassName= 'skills__name'>dd</h3>
-            <span className='skills__level'></span>
-        </div>
-        </div>
-    
-
-     <div className="skills__data">
-        <i className='bx bxs-badge-check'></i>
-        <div>
-            <h3 ClassName= 'skills__name'>dd</h3>
-            <span className='skills__level'></span>
-        </div>
-        </div>
-    
-            </div>
-
-            <div className="skills__group">
-    <div className="skills__data">
-        <i className='bx bxs-badge-check'></i>
-        <div>
-            <h3 ClassName= 'skills__name'>dd</h3>
-            <span className='skills__level'></span>
-        </div>
-        </div>
-   
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 className= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
 
 
-     <div className="skills__data">
-        <i className='bx bxs-badge-check'></i>
-        <div>
-            <h3 ClassName= 'skills__name'>dd</h3>
-            <span className='skills__level'></span>
-        </div>
-        </div>
-    
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 className= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
 
-     <div className="skills__data">
-        <i className='bx bxs-badge-check'></i>
-        <div>
-            <h3 ClassName= 'skills__name'>dd</h3>
-            <span className='skills__level'></span>
-        </div>
-        </div>
-    
 
-     <div className="skills__data">
-        <i className='bx bxs-badge-check'></i>
-        <div>
-            <h3 ClassName= 'skills__name'>dd</h3>
-            <span className='skills__level'></span>
-        </div>
-        </div>
-    
-            </div>
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 className= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
 
-        </div>
-        </div>
+    </div>
+
+    <div className="skills__group">
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 className= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
+
+
+
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 className= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
+
+
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 className= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
+
+
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 className= 'skills__name'>dd</h3>
+    <span className='skills__level'></span>
+</div>
+</div>
+
+    </div>
+
+</div>
+</div>
   
   )
 }
