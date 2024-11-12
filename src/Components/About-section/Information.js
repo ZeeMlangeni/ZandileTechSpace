3583bf53f281fe968c7d7aa5b22bc835f08b3e4f
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-12 14:55:42 +0200

    changed

--- src/Components/About-section/Information.js
+++ src/Components/About-section/Information.js
@@ -1,27 +1,34 @@
-import React from 'react'
+import React from 'react';
 
 function Information() {
   return (
-   
-<div className='about__info grid'>
-    <div className='about__box'>
-        <h3 className='about__title'></h3>
-        <span className ='about__subtitle'></span>
-    </div>
+    <div className='about__info grid'>
+      
+      {/* Skills and Goals Section */}
+      <div className='about__box'>
+        <h3 className='about__title'>Skills and Goals</h3>
+        <span className='about__subtitle'>
+          <i className="uil uil-brackets-curly"></i> Intermediate skills in HTML, CSS, JavaScript, and React.
+        </span>
+      </div>
 
-    <div className='about__box'>
-        <h3 className='about__title'></h3>
-        <span className ='about__subtitle'></span>
-    </div>
+      {/* Education Section */}
+      <div className='about__box'>
+        <h3 className='about__title'>Education</h3>
+        <span className='about__subtitle'>
+          <i className="uil uil-graduation-cap"></i> Currently pursuing a Diploma in Information Technology.
+        </span>
+      </div>
 
-    <div className='about__box'>
-        <h3 className='about__title'></h3>
-        <span className ='about__subtitle'></span>
+      {/* Future Goals Section */}
+      <div className='about__box'>
+        <h3 className='about__title'>Future Goals</h3>
+        <span className='about__subtitle'>
+          <i className="uil uil-server"></i> Exploring Node.js and full-stack development.
+        </span>
+      </div>
     </div>
-</div>
-
-   
-  )
+  );
 }
 
-export default Information
+export default Information;
