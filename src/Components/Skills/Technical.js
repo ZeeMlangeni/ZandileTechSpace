535be170ae4269a77c4ba2ba3632497cfdd67b01
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-12 13:19:36 +0200

    Update technical deleted

--- src/Components/Skills/Technical.js
+++ src/Components/Skills/Technical.js
@@ -45,7 +45,7 @@ function Technical() {
           <div className="skills__data">
             <i className="bx bxs-badge-check"></i>
             <div>
-              <h3 className="skills__name">SQLDB</h3>
+              <h3 className="skills__name">SQL</h3>
               <span className="skills__level">Intermediate</span>
             </div>
           </div>
@@ -58,13 +58,7 @@ function Technical() {
             </div>
           </div>
 
-          <div className="skills__data">
-            <i className="bx bxs-badge-check"></i>
-            <div>
-              <h3 className="skills__name">Python</h3>
-              <span className="skills__level">Basic</span>
-            </div>
-          </div>
+        
 
     
 
@@ -76,13 +70,7 @@ function Technical() {
             </div>
           </div>
 
-          <div className="skills__data">
-            <i className="bx bxs-badge-check"></i>
-            <div>
-              <h3 className="skills__name">Visual Studio Code</h3>
-              <span className="skills__level">Intermediate</span>
-            </div>
-          </div>
+         
         </div>
       </div>
     </div>
