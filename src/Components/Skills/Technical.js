e5782f56e63264887cedc7b595a48876b8412743
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-12 12:02:59 +0200

    updated technical

--- src/Components/Skills/Technical.js
+++ src/Components/Skills/Technical.js
@@ -6,10 +6,12 @@ function Technical() {
 
 
         <div className="skills__content">
-            <h3 className='skills__title'></h3>
+            <h3 className='skills__title'>nn</h3>
             <div className='skills__box'>
 <div className="skills__group">
-    <div className="skills__data"></div>
+    <div className="skills__data">
+        <i className='bx bxs-badge-check'></i>
+        </div>
      </div>
             </div>
 
