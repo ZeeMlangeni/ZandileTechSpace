95d55dba0e5374a03967a663aa97b9eef0f23060
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-12 14:02:01 +0200

    removed

--- src/Components/About-section/Section.js
+++ src/Components/About-section/Section.js
@@ -37,34 +37,7 @@ function Section() {
 
 
 
-      <div >
-        <p>
-          I began my coding journey in 2022 and have developed my skills to an intermediate level in HTML, CSS, JavaScript, and React. I focus on creating intuitive, visually appealing sites with clean, maintainable code.<br /><br />
-          Quick Bits About Me:
-        </p>
-        <ul className='about-list'>
-          <li className='nav_item'>
-            <i className="uil uil-arrow"></i> 
-            Intermediate skills in HTML, CSS, JavaScript, and React.
-          </li>
-          <li className='nav_item'>
-            <i className="uil uil-graduation-cap"></i>
-            Currently pursuing a Diploma in Information Technology.
-          </li>
-          <li className='nav_item'>
-            <i className="uil uil-server"></i>
-            Exploring Node.js and full-stack development.
-          </li>
-        </ul>
-        
-        <p className='about-message'>
-          I’m available for freelance work and collaborations. Feel free to reach out, I’d love to connect!
-        </p>
-      </div>
-      <button className='cv-btn'>
-  <a href="/ZandileCV.pdf" target="_blank" rel="noopener noreferrer">Download CV</a>
-</button>
-
+      
     </div>
   );
 }
