606b17439e0828979639a873225005d0dfa10232
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-12 12:28:07 +0200

    Update technical

--- src/Components/Skills/Technical.js
+++ src/Components/Skills/Technical.js
@@ -6,24 +6,15 @@ function Technical() {
 
 
     <div className="skills__content">
-    <h3 className='skills__title'>nn</h3>
+    <h3 className='skills__title'>Technical Skills</h3>
     <div className='skills__box'>
 <div className="skills__group">
-<div className="skills__data">
-<i className='bx bxs-badge-check'></i>
-<div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
-</div>
-</div>
-
-
 
 <div className="skills__data">
 <i className='bx bx-badge-check'></i>
 <div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
+    <h3 className= 'skills__name'>HTML</h3>
+    <span className='skills__level'>intermediate</span>
 </div>
 </div>
 
@@ -31,8 +22,8 @@ function Technical() {
 <div className="skills__data">
 <i className='bx bxs-badge-check'></i>
 <div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
+    <h3 className= 'skills__name'>CSS</h3>
+    <span className='skills__level'>Intermediate</span>
 </div>
 </div>
 
@@ -40,8 +31,8 @@ function Technical() {
 <div className="skills__data">
 <i className='bx bxs-badge-check'></i>
 <div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
+    <h3 className= 'skills__name'>JavaScript</h3>
+    <span className='skills__level'>Intermediate</span>
 </div>
 </div>
 
@@ -51,8 +42,8 @@ function Technical() {
 <div className="skills__data">
 <i className='bx bxs-badge-check'></i>
 <div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
+    <h3 className= 'skills__name'>React</h3>
+    <span className='skills__level'>Basic</span>
 </div>
 </div>
 
@@ -61,8 +52,8 @@ function Technical() {
 <div className="skills__data">
 <i className='bx bxs-badge-check'></i>
 <div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
+    <h3 className= 'skills__name'>SQLDB</h3>
+    <span className='skills__level'>Intermediate</span>
 </div>
 </div>
 
@@ -70,8 +61,32 @@ function Technical() {
 <div className="skills__data">
 <i className='bx bxs-badge-check'></i>
 <div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
+    <h3 className= 'skills__name'>C#</h3>
+    <span className='skills__level'>Intermediate</span>
+</div>
+</div>
+
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 className= 'skills__name'>Python</h3>
+    <span className='skills__level'>basic</span>
+</div>
+</div>
+
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 className= 'skills__name'>Visual Basic</h3>
+    <span className='skills__level'>Beginner</span>
+</div>
+</div>
+
+<div className="skills__data">
+<i className='bx bxs-badge-check'></i>
+<div>
+    <h3 className= 'skills__name'>Figma</h3>
+    <span className='skills__level'>Good</span>
 </div>
 </div>
 
@@ -79,8 +94,8 @@ function Technical() {
 <div className="skills__data">
 <i className='bx bxs-badge-check'></i>
 <div>
-    <h3 className= 'skills__name'>dd</h3>
-    <span className='skills__level'></span>
+    <h3 className= 'skills__name'>Visual Studio Code</h3>
+    <span className='skills__level'>Intermediate</span>
 </div>
 </div>
 
