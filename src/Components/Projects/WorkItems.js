87a23ef68eb482a6432ae9229a9a3b6699b6e628
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-13 21:41:24 +0200

    changes were made

--- src/Components/Projects/WorkItems.js
+++ src/Components/Projects/WorkItems.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import './WorkItems.css'; // Make sure to create or update this CSS file with the styles below
-import './WorkItems'
 
 const WorkItems = ({ item }) => {
   return (
-    <div className='work__card' key={item.id}>
-      <div className="card-inner">
-        {/* Front of the card */}
-        <div className="card-front">
-          <img src={item.image} alt="product-image" className="work__img" />
-          <h3 className="work__title">{item.name}</h3>
-        </div>
-        {/* Back of the card */}
-        <div className="card-back">
-          <p>{item.description}</p>
-          <a href={item.github} target="_blank" rel="noopener noreferrer" className="work__button">
-            Git Repo <i className="uil uil-arrow-right work__button-icon"></i>
-          </a>
-        </div>
+    <div className='card-inner'>
+      <div className='work__card' key={item.id}>
+        <img src={item.image} alt='product-image' className='work__img'/>
+        <h3 className='work__title'>{item.name}</h3>
+        <a href="#" className='work__button'>
+          Git Repo <i className="uil uil-arrow-right work__button-icon"></i>
+        </a>
       </div>
     </div>
   );
-};
+}
 
 export default WorkItems;
