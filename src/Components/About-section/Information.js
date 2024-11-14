f59130c332b540cc5fcdb9fb051c5a6101261ef4
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-14 22:27:00 +0200

    updated information

--- src/Components/About-section/Information.js
+++ src/Components/About-section/Information.js
@@ -1,34 +1,31 @@
-import React from 'react';
+import React from "react";
 
 function Information() {
   return (
-    <div className='about__info grid'>
-      
+    <div className="about__info grid">
       {/* Skills and Goals Section */}
-      <div className='about__box'>
-      <i className="uil uil-brackets-curly about__icon"></i>
-        <h3 className='about__title'>Skills and Goals</h3>
-        <span className='about__subtitle'>
+      <div className="about__box">
+        <i className="uil uil-brackets-curly about__icon"></i>
+        <h3 className="about__title">Skills and Goals</h3>
+        <span className="about__subtitle">
           Intermediate skills in Front-end Languages.
         </span>
       </div>
 
       {/* Education Section */}
-      <div className='about__box'>
-      <i className="uil uil-graduation-cap about__icon "></i>
-        <h3 className='about__title'>Education</h3>
-        <span className='about__subtitle'>
+      <div className="about__box">
+        <i className="uil uil-graduation-cap about__icon "></i>
+        <h3 className="about__title">Education</h3>
+        <span className="about__subtitle">
           Diploma in Information Technology.
         </span>
       </div>
 
       {/* Future Goals Section */}
-      <div className='about__box'>
-      <i className="uil uil-server about__icon "></i>
-        <h3 className='about__title'>Future Goals</h3>
-        <span className='about__subtitle'>
-           Exploring Node.js .
-        </span>
+      <div className="about__box">
+        <i className="uil uil-server about__icon "></i>
+        <h3 className="about__title">Future Goals</h3>
+        <span className="about__subtitle">Exploring Node.js .</span>
       </div>
     </div>
   );
