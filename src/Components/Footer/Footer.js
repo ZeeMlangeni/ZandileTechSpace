abacb59074a4b57099b9f496676c02ebdf28bdff
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-14 23:34:14 +0200

    styling

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,60 +1,73 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <footer className="footer section " >
-      <div className="footer-content">
-        {/* Social Media Icons */}
-        <div className="home-social">
-          <ul className="social-icons">
-            <li>
-              <a
-                href="https://github.com"
-                className="home-social-icon"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <i className="uil uil-github"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com"
-                className="home-social-icon"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <i className="uil uil-linkedin"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.instagram.com/zeeicygirl/"
-                className="home-social-icon"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <i className="uil uil-instagram-alt"></i>
-              </a>
-            </li>
-          </ul>
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Zandi</h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#hero" className="footer__link">
+              Home
+            </a>
+          </li>
+
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#skills" className="footer__link">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="footer__link">
+              CV
+            </a>
+          </li>
+          <li>
+            <a href="#form" className="footer__link">
+              Contact
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a
+            href="https://github/zeeMlangeni"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="uil uil-github"></i>
+          </a>
+
+          <a
+            href="https://www.linkedin.com/feed/"
+           className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="uil uil-linkedin"></i>
+          </a>
+
+          <a
+            href="https://www.instagram.com/zeeicygirl/"
+        className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="uil uil-instagram-alt"></i>
+          </a>
         </div>
 
-        {/* Navigation Links */}
-        <div className="footer-links">
-          <ul className="link-list">
-            <li><a href="example.com" className="links">Home</a></li>
-            <li><a href="example.com" className="links">About</a></li>
-            <li><a href="example.com" className="links">Skills</a></li>
-            <li><a href="example.com" className="links">CV</a></li>
-            <li><a href="#example.com" className="links">Contact</a></li>
-          </ul>
-        </div>
-
-        {/* Copyright */}
-        <hr/>
-        <p className="copyright">© 2024 Zandile. All rights reserved.</p>
+        <span className="footer__copy">
+       © 2024 Zandile. All rights reserved.</span>
       </div>
     </footer>
   );
