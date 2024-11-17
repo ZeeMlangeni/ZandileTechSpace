9a768d53519d0d1d68993026ed729e10f4e65e66
Zandile Mlangeni <mlangenizandile84@gmail.com>
2024-11-17 03:12:21 +0200

    changed styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,27 +4,26 @@ import "./Header.css";
 import DarkMode from "./DarkMode.js";
 
 function Header() {
-  
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
 
   //This function is the one that enables smooth scrolling to a specific element on the page.
   const handleSmoothScroll = (id) => {
     const element = document.getElementById(id);
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: "smooth" });
     }
-    setIsMenuOpen(false); // Close the menu after scrolling
+    setIsMenuOpen(false);
   };
 
-
+  // The toggleMenu function toggles the state of the menu's visibility.
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  //My header component
   return (
-    <header className="header " >
+    <header className="header ">
       <nav className="nav container">
         <a href="index.html" className="nav logo">
           <h2>
@@ -32,51 +31,71 @@ function Header() {
           </h2>
         </a>
         <div className="nav-menu">
-          <ul className={`nav-list grid ${isMenuOpen ? "active" : ""}`}> 
+          <ul className={`nav-list grid ${isMenuOpen ? "active" : ""}`}>
             <li className="nav__item">
-              <a className="nav__link" href="#hero" onClick={() => setIsMenuOpen(false)}>
+              <a
+                className="nav__link"
+                href="#hero"
+                onClick={() => setIsMenuOpen(false)}
+              >
                 Home
               </a>
             </li>
             <li className="nav__item">
-              <a className="nav__link" href="#about" onClick={() => {
-      handleSmoothScroll('projects');
-      setIsMenuOpen(false);
-    }}>
+              <a
+                className="nav__link"
+                href="#about"
+                onClick={() => {
+                  handleSmoothScroll("projects");
+                  setIsMenuOpen(false);
+                }}
+              >
                 About
               </a>
             </li>
             <li className="nav__item">
-              <a className="nav__link" href="#skills" onClick={() => setIsMenuOpen(false)}>
+              <a
+                className="nav__link"
+                href="#skills"
+                onClick={() => setIsMenuOpen(false)}
+              >
                 Skills
               </a>
             </li>
 
             <li className="nav__item">
-              <a className="nav__link" href="#projects"  onClick={() => {
-      handleSmoothScroll('projects');
-      setIsMenuOpen(false);
-    }} >
-               Projects
+              <a
+                className="nav__link"
+                href="#projects"
+                onClick={() => {
+                  handleSmoothScroll("projects");
+                  setIsMenuOpen(false);
+                }}
+              >
+                Projects
               </a>
             </li>
             <li className="nav__item">
-              <a className="nav__link" href="#about" onClick={() => setIsMenuOpen(false)}>
+              <a
+                className="nav__link"
+                href="#about"
+                onClick={() => setIsMenuOpen(false)}
+              >
                 CV
               </a>
             </li>
-          
-       <li className="nav__item"  onClick={() => setIsMenuOpen(false)}>
-      
 
-       <DarkMode/>
-     
-    {/*    <Switch /> */}
-    
-       </li>
+            <li className="nav__item" onClick={() => setIsMenuOpen(false)}>
+              <DarkMode />
+
+              {/*    <Switch /> */}
+            </li>
           </ul>
-          <div className="nav__toggle" onClick={toggleMenu}> 
-            <i className={`uil ${isMenuOpen ? 'uil-times' : 'uil-bars'}`} nav___icon></i> 
+          <div className="nav__toggle" onClick={toggleMenu}>
+            <i
+              className={`uil ${isMenuOpen ? "uil-times" : "uil-bars"}`}
+              nav___icon
+            ></i>
           </div>
         </div>
       </nav>
